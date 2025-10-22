0703501d5a56d36d047cc851a94269411bce0361
cjaawesome <69875824+cjaawesome@users.noreply.github.com>
2025-10-22 14:30:19 -0500

    BlockBuffer methods

--- src/BlockBuffer.cpp
+++ src/BlockBuffer.cpp
@@ -1,1 +1,20 @@
 #include "BlockBuffer.h"
+
+
+
+bool BlockBuffer::openFile(const std::string& filename, const size_t headerSize){
+    blockFile.open(filename, std::ios::binary);
+    if (!blockFile) { //if file couldn't open set error
+        setError("Error opening file!");
+        errorState = true;
+        return false;
+    }
+    blockFile.seekg(headerSize); //skip header
+
+    return true;
+}
+
+bool BlockBuffer::hasMoreData() const{
+    return blockFile.is_open() && !blockFile.eof() && !errorState;
+}
+
