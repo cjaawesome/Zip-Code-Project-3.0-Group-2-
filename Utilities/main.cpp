c0e330305c8b69a0681875588f4625c68b6a90db
cjaawesome <69875824+cjaawesome@users.noreply.github.com>
2025-11-18 23:20:12 -0600

    updated main

--- Utilities/main.cpp
+++ Utilities/main.cpp
@@ -1,23 +1,7 @@
 #include "ZipSearchApp.h"
 #include <iostream>
 
-int main(int argc, char* argv[]) 
-{
-    if (argc < 3) 
-    {
-        std::cout << "Usage: " << argv[0] << " <datafile.zcd> -Z<zipcode> [-Z<zipcode> ...]\n";
-        std::cout << "Example: " << argv[0] << " NotRandomCSV.zcd -Z96737 -Z97134\n";
-        return 1;
-    }
-    
-    std::string dataFile = argv[1];
-    
-    ZipSearchApp app(dataFile);
-    
-    if (!app.search(argc, argv)) {
-        std::cerr << "Search failed.\n";
-        return 1;
-    }
-    
-    return 0;
+int main(int argc, char* argv[]){
+    ZipSearchApp app;
+    app.process(argc, argv);
 }
